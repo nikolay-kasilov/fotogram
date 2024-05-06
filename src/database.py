0a33cf26bf5daa23f7b54ebbe60c4dd5e2eff765
Your Name <you@example.com>
2024-05-06 19:55:13 +0300

    add engine in database py

--- src/database.py
+++ src/database.py
@@ -1,5 +1,6 @@
 """Файл для описания объекта sqlalchemy базы данных."""
 from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
 from settings import settings
 
@@ -7,3 +8,8 @@ engine = create_engine(f"{settings.DB_DIALECT}://"
                        f"{settings.DB_HOST}:{settings.DB_PORT}@"
                        f"{settings.DB_USER}:{settings.DB_PASSWORD}/"
                        f"{settings.DB_NAME}")
+
+session_factory = sessionmaker(bind=engine)
+
+class Base(DeclarativeBase):
+    """Базовая модель."""
